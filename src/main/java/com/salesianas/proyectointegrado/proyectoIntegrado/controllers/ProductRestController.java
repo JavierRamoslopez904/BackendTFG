1e98becab86b8893a3b045611b6f91adb44fc695
Javier <javier.ramoslopez@nervion.salesianas.org>
2022-06-12 12:29:49 +0200

    BackendTFG_JavierRamosLopez

--- src/main/java/com/salesianas/proyectointegrado/proyectoIntegrado/controllers/ProductRestController.java
+++ src/main/java/com/salesianas/proyectointegrado/proyectoIntegrado/controllers/ProductRestController.java
@@ -90,9 +90,13 @@ public class ProductRestController {
 		// Método para encontrar un producto por su id
 		Product product = productService.findById(id);
 
+		// Vemos si el archivo no está vacío
 		if (!archivo.isEmpty()) {
+			// Obtención de la foto que el usuario está intentando subir
 			String nombreArchivo = UUID.randomUUID().toString() + " _ "
 					+ archivo.getOriginalFilename().replace(" ", " ");
+			
+			// Almacenamiento en una variable de tipo Path, el nombre del archivo 
 			Path rutaArchivo = Paths.get("upload").resolve(nombreArchivo).toAbsolutePath();
 
 			try {
@@ -104,19 +108,28 @@ public class ProductRestController {
 				return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 
+			// Almacenamiento en una variable de tipo String la foto del product
 			String nombreFotoAnterior = product.getFoto();
 
+			// Establecimiento de condiciones para ver si la foto existe
 			if (nombreFotoAnterior != null && nombreFotoAnterior.length() > 0) {
+				
+				// Almacenamiento en una variable de tipo Path la ruta de la foto anterior
 				Path rutaFotoAnterior = Paths.get("uploads").resolve(nombreFotoAnterior).toAbsolutePath();
+				
+				// Almacenamiento en una variable de tipo File, el archivo anterior
 				File archivoFotoAnterior = rutaFotoAnterior.toFile();
 
+				// Establecimiento de condiciones para ver si hay un archivo existente, si es así lo elimina
 				if (archivoFotoAnterior.exists() && archivoFotoAnterior.canRead()) {
 					archivoFotoAnterior.delete();
 				}
 			}
 
+			// Modificamos la foto para pasarle la nueva foto
 			product.setFoto(nombreArchivo);
 
+			// Añadimos el nuevo producto, con la foto actualizada
 			productService.addProduct(product);
 
 			response.put("user", product);
@@ -128,21 +141,30 @@ public class ProductRestController {
 
 	}
 
+	/**
+	 * Método para ver la foto correspondiente a cada producto
+	 * @param nombreFoto
+	 * @return
+	 */
 	@GetMapping("/upload/img/{nombreFoto:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto) {
 
+		// Almacenamiento en una variable de tipo Path, del nombre de la foto
 		Path rutaArchivo = Paths.get("upload").resolve(nombreFoto).toAbsolutePath();
 
 		Resource recurso = null;
 
 		try {
+			// Asignación del nombre del archivo a una variable de tipo Resource llamada recurso
 			recurso = new UrlResource(rutaArchivo.toUri());
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 
+		// Establecimiento de condiciones para ver si la foto existe
 		if (!recurso.exists() && !recurso.isReadable()) {
-			rutaArchivo = Paths.get("src/main/resources/static/images").resolve("nouser.png").toAbsolutePath();
+			// Si no existe, muestra la imagen por defecto
+			rutaArchivo = Paths.get("src/main/resources/static/images").resolve("cruz.jpg").toAbsolutePath();
 
 			try {
 				recurso = new UrlResource(rutaArchivo.toUri());
@@ -151,6 +173,7 @@ public class ProductRestController {
 			}
 		}
 
+		// Configuración de la cabecera que es necesaria para enviar el resultado
 		HttpHeaders cabecera = new HttpHeaders();
 		cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
 
